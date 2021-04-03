693a6327e331638411574e0c63ef299f77c6aec1
FishBowl025 <dvs025@gmail.com>
2021-04-03 12:09:33 -0400

    Added examples

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import deepltranslator
 
 
 translator = deepltranslator.DeeplTranslator()
-print("Single sentence:")
+print("Single sentence, using default languages:")
 print(translator.get_translation("Hello"))
+print("Single sentence, using languages different from default:")
+print(translator.get_translation("Bonjour", source_lang='fr', target_lang='en'))
 print("List of sentences:")
 print(translator.get_translation(lst=["Hello", "How are you?"]))
-
